7555b4a82db73b1c1b737ffbb1f826ff1b2009a6
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-01-15 17:43:36 -0800

    :zap: export new components

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,19 @@ import Paragraph from "./components/Text/Paragraph/Paragraph";
 import Text from "./components/Text/Text";
 import Stack from "./components/Stack/Stack";
 import { useAppStore } from "./store/app";
+import Button from "./components/Button/Button";
+import ColorScheme from "./components/ColorScheme/ColorScheme";
+import Input from "./components/Input/Input";
 
-export { AppWrapper, Box, Heading, Paragraph, Text, Stack, useAppStore };
+export {
+  AppWrapper,
+  Button,
+  Box,
+  ColorScheme,
+  Heading,
+  Input,
+  Paragraph,
+  Text,
+  Stack,
+  useAppStore,
+};
